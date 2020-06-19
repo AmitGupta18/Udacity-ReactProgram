604df33a9890bd7ba2e456e296411dfb8ce8431d
Amit Gupta <agupta@thinkanalytics.com>
2020-06-20 02:36:06 +0530

    feat Styled component in DateHeader component

--- Exercise/Module5_UdaciFitness/UdaciFitness/components/DateHeader.js
+++ Exercise/Module5_UdaciFitness/UdaciFitness/components/DateHeader.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Text } from "react-native";
 import { purple } from "../utils/colors";
+import styled from "styled-components/native";
+
+const DateHeaderText = styled.Text`
+  font-size: 25;
+  color: #292477;
+`;
 
 export default function DateHeader({ date }) {
-  return <Text style={{ fontSize: 25, color: purple }}>{date}</Text>;
+  return <DateHeaderText>{date}</DateHeaderText>;
 }
